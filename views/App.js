179627ec3233bcfbb3b4c81abeeadac2b0e4c106
Fehér Máté <94060600+Fehermate1@users.noreply.github.com>
2024-11-18 08:34:57 +0100

    Update App.js - 2. Feladat kész

--- views/App.js
+++ views/App.js
@@ -1,10 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fs from "fs";
+import { Router } from "express";
 
 const App = express();
 const PORT = 3000;
 
 App.listen(3000, () => {
     console.log("Hi There")
+});
+
+App.get("/express", (req, res,) => {
+    console.log("Az Express egy minimalista webes keretrendszer, amely a Node.js-hez készült.")
+});
+
+App.get("/greeting", (req, res) => {
+    console.log("Hello, Fehér Máté")
+});
+
+App.get("nodejs ", (req, res) => {
+    console.log("A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül.")
 });
