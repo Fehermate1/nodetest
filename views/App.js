5b6564d27296bd4556a84973a743b3c67efbb346
Fehér Máté <94060600+Fehermate1@users.noreply.github.com>
2024-11-18 08:36:00 +0100

    Update App.js - 2. Feladat kész

--- views/App.js
+++ views/App.js
@@ -5,18 +5,22 @@ import { Router } from "express";
 const App = express();
 const PORT = 3000;
 
+App.get("/", (req, res) =>{
+    console.log("A szerver fut");
+});
+
 App.listen(3000, () => {
-    console.log("Hi There")
+    console.log("Hi There");
 });
 
 App.get("/express", (req, res,) => {
-    console.log("Az Express egy minimalista webes keretrendszer, amely a Node.js-hez készült.")
+    console.log("Az Express egy minimalista webes keretrendszer, amely a Node.js-hez készült.");
 });
 
 App.get("/greeting", (req, res) => {
-    console.log("Hello, Fehér Máté")
+    console.log("Hello, Fehér Máté");
 });
 
 App.get("nodejs ", (req, res) => {
-    console.log("A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül.")
+    console.log("A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül.");
 });
